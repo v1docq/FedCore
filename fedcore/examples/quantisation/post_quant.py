6d2f451cb039ae6a0e9874db7999f83c0f787764
v1docq <ir@raxeltelematics.com>
2024-02-22 15:44:02 +0300

    refactor quant

--- fedcore/examples/quantisation/post_quant.py
+++ fedcore/examples/quantisation/post_quant.py
@@ -1,17 +1,11 @@
 import numpy as np
 import torch.nn
 import torchvision.datasets
-
-
 from torch.utils.data import DataLoader
 from torchvision.models import resnet18
 from torchvision.transforms import transforms
-
-
 from fedcore.architecture.utils.paths import data_path
 
-from fedcore.neural_compressor import PostTrainingQuantConfig
-from fedcore.neural_compressor import quantization
 
 if __name__ == '__main__':
     transform = transforms.Compose(
@@ -25,22 +19,4 @@ if __name__ == '__main__':
                                                 transform=transform)
     val_dataset, test_dataset = torch.utils.data.random_split(val_dataset, [0.1, 0.9])
 
-    val_dataloader = DataLoader(
-        val_dataset,
-        batch_size=64,
-        shuffle=False,
-        num_workers=1
-    )
-
-
-    # Quantization code
-
-    conf = (
-        PostTrainingQuantConfig()
-    )  # default approach is "auto", you can set "dynamic":PostTrainingQuantConfig(approach="dynamic")
-    q_model = quantization.fit(
-        model=model,
-        conf=conf,
-        calib_dataloader=val_dataloader
-    )
-    q_model.save("./output")
+    val_dataloader = DataLoader(val_dataset, batch_size=64, shuffle=False, num_workers=1)
