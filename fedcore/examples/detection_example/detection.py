49580185bb5d6586401eab55e4e9e69641a45a4c
PvtKaefsky <kaefsky@gmail.com>
2024-07-07 02:37:00 +0300

    Detection example fixes

--- fedcore/examples/detection_example/detection.py
+++ fedcore/examples/detection_example/detection.py
@@ -17,7 +17,7 @@ from fedcore.architecture.utils.loader import collate
 from fedcore.architecture.visualisation.visualization import show_image
 
 DATASET_NAME = 'african-wildlife'
-EPOCHS = 5
+EPOCHS = 25
 
 if torch.cuda.is_available():
     print(torch.cuda.get_device_name(0))
@@ -32,6 +32,9 @@ if __name__ == '__main__':
     
     device = default_device()
     
+    # If dataset doesn't exist, it will be downloaded from 
+    # https://docs.ultralytics.com/datasets/detect/#supported-datasets
+    # (large datasets like COCO can't be downloaded directly)
     train_dataset = YOLODataset(dataset_name=DATASET_NAME, transform=transform, train=True)
     train_loader = DataLoader(
         train_dataset, 
@@ -55,61 +58,78 @@ if __name__ == '__main__':
         collate_fn=collate
     )
 
+    # Define model and number of classes
     model = fasterrcnn_mobilenet_v3_large_fpn(pretrained=True)
     num_classes = len(train_dataset.classes)
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes).to(device)
     model.to(device)
     
+    # Define the optimizer, scheduler and evaluator
     opt = optim.SGD(model.parameters(), lr=4e-5)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(opt, patience=3, verbose=True)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(opt, mode='max', patience=3, verbose=True)
     evaluator = PerformanceEvaluatorOD(model, test_loader, batch_size=1)
     
     train_loss = list()
-    val_loss = list()
+    val_loss = list() 
     
-    model.train()   
-    # Train the model
     for epoch in range(EPOCHS):
+        # Train the model
         model.train()
         running_loss = 0.0
         epoch_loss = np.zeros(len(train_loader))
+        
         for i, (images, targets) in enumerate(train_loader):
+            # forward
             loss_dict = model(images, targets)
             loss = sum(loss for loss in loss_dict.values())
+            
+            # backward + optimize
             opt.zero_grad()
             loss.backward()
             opt.step() 
+            
+            # Print loss
             running_loss += loss.item()
             if i % 50 == 0:    
                 print('[%d, %5d] loss: %.3f' %
                         (epoch + 1, i + 1, running_loss / 50))
                 running_loss = 0.0
-            epoch_loss[i] = loss
-        if device == 'cuda':
-            torch.cuda.empty_cache()
-        train_loss.append(epoch_loss.mean())
+            epoch_loss[i] = loss               
+        
+        # Evaluate the model
         model.eval()
         target_metric = evaluator.measure_target_metric()
+        scheduler.step(float(target_metric["map"]))
+        
         print('[%d] MAP: %.3f' %
                     (epoch + 1, target_metric["map"]))
-        scheduler.step(float(target_metric["map"]))
+        
+        train_loss.append(epoch_loss.mean())
         val_loss.append(float(target_metric["map"]))
-        if len(val_loss) > 4 and val_loss[-1] == val_loss[-5]:
+        
+        if device == 'cuda':
+            torch.cuda.empty_cache()
+            
+        if len(val_loss) > 5 and val_loss[-1] <= val_loss[-3]:
             print("Early stopping")
             break
+    
+    # Final evaluating
     performance = evaluator.eval()
     print('Before quantization')
     print(performance)
     torch.save(model, f'{model._get_name()}_' + DATASET_NAME + '.pt')
     
+    # Inference
     model.cpu()
-    val_data = val_dataset[random.randint(0, len(val_dataset) - 1)]
+    val_data = val_loader.dataset[random.randint(0, len(val_dataset) - 1)]
     img = val_data[0]
     targets = val_data[1]
     input = torch.unsqueeze(img, dim=0)
     preds = model(input)
 
+    # Show inference image
     transform = v2.ToPILImage()
     img = transform(img)
     show_image(img, targets, preds, train_dataset.classes)
