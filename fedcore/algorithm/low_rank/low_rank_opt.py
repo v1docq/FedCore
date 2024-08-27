209b9e642e3efc22db11af2b8dc13e38158ced5e
v1docq <ir@raxeltelematics.com>
2024-08-27 14:06:30 +0300

    refactor custom loss, add logging

--- fedcore/algorithm/low_rank/low_rank_opt.py
+++ fedcore/algorithm/low_rank/low_rank_opt.py
@@ -1,3 +1,5 @@
+from functools import reduce
+from operator import iadd
 from typing import Dict, List, Optional
 
 from fedot.core.data.data import InputData
@@ -9,6 +11,7 @@ from fedcore.algorithm.low_rank.svd_tools import load_svd_state_dict, decompose_
 from fedcore.losses.low_rank_loss import HoyerLoss, OrthogonalLoss
 from fedcore.losses.utils import _get_loss_metric
 from fedcore.models.network_impl.layers import DecomposedConv2d
+from fedcore.neural_compressor.compression.pruner.utils import nn
 from fedcore.repository.constanst_repository import ENERGY_THR, DECOMPOSE_MODE, FORWARD_MODE, HOER_LOSS, ORTOGONAL_LOSS
 import torch
 from fedcore.repository.constanst_repository import default_device
@@ -30,9 +33,9 @@ class LowRankModel:
     def __init__(
             self, params: Optional[OperationParameters] = {}):
         self.epochs = params.get('epochs', 5)
-        self.energy_thresholds = params.get('epochs', ENERGY_THR)
-        self.decomposing_mode = params.get('epochs', DECOMPOSE_MODE)
-        self.forward_mode = params.get('epochs', FORWARD_MODE)
+        self.energy_thresholds = params.get('energy_thresholds', ENERGY_THR)
+        self.decomposing_mode = params.get('decomposing_mode', DECOMPOSE_MODE)
+        self.forward_mode = params.get('forward_mode', FORWARD_MODE)
         self.hoer_loss = HoyerLoss(params.get('hoyer_loss', HOER_LOSS))
         self.orthogonal_loss = OrthogonalLoss(params.get('orthogonal_loss', ORTOGONAL_LOSS))
         self.learning_rate = params.get('learning_rate', 0.001)
@@ -42,6 +45,9 @@ class LowRankModel:
     def _init_model(self, input_data):
         self.loss_fn = _get_loss_metric(input_data)
         self.model = input_data.target
+        self.model.fc = nn.Sequential(
+            nn.Linear(self.model.fc.in_features, input_data.features.num_classes)
+        )
         decompose_module(self.model, self.decomposing_mode,
                          forward_mode=self.forward_mode)
         self.model.to(self.device)
@@ -54,7 +60,6 @@ class LowRankModel:
                   custom_loss: dict = None):
 
         total_iterations = 0
-
         for epoch in range(1, self.epochs + 1):
             loss_sum = 0
             self.model.train()
@@ -65,14 +70,19 @@ class LowRankModel:
                 output = self.model(inputs.to(self.device))
                 if custom_loss:
                     model_loss = {key: val(model) for key, val in custom_loss.items()}
-                    quality_loss = self.loss_fn(output, targets.to(self.device))
-                    loss_sum += sum([loss.item() for loss in model_loss.values()]) + quality_loss.item()
+                    model_loss['metric_loss'] = self.loss_fn(torch.argmax(output, dim=1).float(),
+                                                targets.to(self.device).float())
+                    loss_sum += sum([loss.item() for loss in model_loss.values()])
+                    quality_loss = reduce(iadd, [loss for loss in model_loss.values()])
                 else:
                     quality_loss = self.loss_fn(output, targets)
                     loss_sum += quality_loss.item()
                 avg_loss = loss_sum / total_iterations
                 quality_loss.backward()
                 self.optimizer.step()
+
+            print('Epoch: {}, {}, Training Loss: {:.2f}'.format(
+                epoch, 'low_rank_loss', avg_loss))
 
             if total_iterations_limit is not None and total_iterations >= total_iterations_limit:
                 return
@@ -84,18 +94,15 @@ class LowRankModel:
         """Run model training with optimization.
 
         Args:
-            model: An instance of the model class
-            params: An object containing training parameters.
-            ft_params: An object containing fine-tuning parameters for optimized model.
+            input_data: An instance of the model class
         """
         self._init_model(input_data)
         self._fit_loop(train_loader=input_data.features.train_dataloader,
                        model=self.model,
                        custom_loss=self.__loss())
-        self.optimize(model=self.model, params=input_data.features, ft_params=input_data.features)
+        return self.optimize(model=self.model, params=input_data.features, ft_params=None)
 
-    def predict_for_fit(self,
-                        input_data: InputData, output_mode: str = 'default'):
+    def predict_for_fit(self,input_data: InputData, output_mode: str = 'default'):
         return self.model
 
     def predict(self,
@@ -119,11 +126,12 @@ class LowRankModel:
         for thr in self.energy_thresholds:
             for name, module in model.named_children():
                 if isinstance(module, DecomposedConv2d):
-                    rank_threshold_pruning(module, thr)
+                    rank_threshold_pruning(module, thr, name)
             if ft_params is not None:
-                pass
-                # train(train_loader=params['train_dataset'], net=model, epochs=params['epoch'],
-                #       custom_loss=self.__loss())
+                self._fit_loop(train_loader=params.train_dataloader,
+                               model=self.model,
+                               custom_loss=self.__loss())
+        return self.model
 
     def __loss(self) -> Dict[str, torch.Tensor]:
         """
