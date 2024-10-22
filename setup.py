07619edc69ddc946201120989b86ece25c96693c
technocreep <aspirantvadim@gmail.com>
2024-10-22 11:25:22 +0200

    created actions and pipy release files

--- setup.py
+++ setup.py
@@ -8,23 +8,22 @@ HERE = Path(__file__).parent.resolve()
 
 # The text of the README file
 NAME = 'fedcore'
-# VERSION = '0.4.2'
-# AUTHOR = 'NSS Lab'
-# AUTHOR_EMAIL = 'itmo.nss.team@gmail.com'
-# SHORT_DESCRIPTION = 'Automated machine learning framework for time series analysis'
-# LONG_DESC_TYPE = 'text/x-rst'
-# README = Path(HERE, 'README_en.rst').read_text(encoding='utf-8')
-EXCLUDED_PACKAGES = ['test*', 'examples', 'benchmark', 'datasets']
-# URL = 'https://github.com/aimclub/Fedot.Industrial'
+VERSION = '0.0.1'
+AUTHOR = 'NSS Lab'
+AUTHOR_EMAIL = 'itmo.nss.team@gmail.com'
+SHORT_DESCRIPTION = 'Federated learning core library'
+LONG_DESC_TYPE = 'text/x-rst'
+README = Path(HERE, 'README.rst').read_text(encoding='utf-8')
+EXCLUDED_PACKAGES = ['tests*', 'examples']
+URL = 'https://github.com/v1docq/FedCore'
 REQUIRES_PYTHON = '>=3.8'
-# LICENSE = 'BSD 3-Clause'
-# KEYWORDS = 'automated machine learning, time series analysis, anomaly detection, classification'
+LICENSE = 'BSD 3-Clause'
+KEYWORDS = 'federated learning, machine learning, deep learning, pruning, quantization, distributed learning'
 
 
 def _readlines(*names: str, **kwargs) -> List[str]:
     encoding = kwargs.get('encoding', 'utf-8')
-    lines = Path(__file__).parent.joinpath(
-        *names).read_text(encoding=encoding).splitlines()
+    lines = Path(__file__).parent.joinpath(*names).read_text(encoding=encoding).splitlines()
     return list(map(str.strip, lines))
 
 
@@ -40,23 +39,23 @@ def _get_requirements(req_name: str):
 
 setuptools.setup(
     name=NAME,
-    # version=VERSION,
-    # author=AUTHOR,
-    # author_email=AUTHOR_EMAIL,
-    # description=SHORT_DESCRIPTION,
-    # long_description=README,
-    # long_description_content_type=LONG_DESC_TYPE,
-    # url=URL,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=SHORT_DESCRIPTION,
+    long_description=README,
+    long_description_content_type=LONG_DESC_TYPE,
+    url=URL,
     python_requires=REQUIRES_PYTHON,
-    # license=LICENSE,
+    license=LICENSE,
     packages=setuptools.find_packages(exclude=EXCLUDED_PACKAGES),
     include_package_data=True,
     install_requires=_get_requirements('requirements.txt'),
-    # classifiers=[
-    #     'License :: OSI Approved :: BSD License',
-    #     'Programming Language :: Python :: 3.8',
-    #     'Programming Language :: Python :: 3.9',
-    #     'Programming Language :: Python :: 3.10',
-    # ],
-    # keywords=KEYWORDS
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords=KEYWORDS
 )
