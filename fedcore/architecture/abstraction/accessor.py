e28b513ea5dd8187de94892078d25524300e2dc0
leostre <strelllleo@mail.ru>
2024-12-23 15:36:10 +0300

    accessor enlargement

--- fedcore/architecture/abstraction/accessor.py
+++ fedcore/architecture/abstraction/accessor.py
@@ -1,5 +1,7 @@
 from functools import reduce
+from typing import Any, List
 
+from torch.ao.quantization.utils import _get_path_of_module
 from torch.nn import Module
 
 
@@ -13,7 +15,59 @@ class Accessor:
         setattr(parent, name, new)
 
     @classmethod
-    def get_module(cls, m: Module, name: str):
+    def get_module(cls, m: Module, name: str) -> Module:
         if not name:
             return m
         return reduce(getattr, name.split('.'), m)
+        
+    def __fetch_names(root: Module, order: list) -> List[str]:
+        names_order = []
+        for submodule in order:
+            names_order.append(_get_path_of_module(root, submodule))
+        return names_order
+    
+    @classmethod
+    def get_names_order(cls, model: Module, *example_input) -> List[str]:
+        modules_order = cls.get_layers_order(model, example_input)
+        names_order = cls.__fetch_names(model, modules_order)
+        return names_order
+    
+    @classmethod
+    def get_layers_order(cls, model: Module, *example_input) -> List[Module]:
+        order = []
+        hooks = []
+        def add_hook(m):
+            def forward_pre_hook(module, input):
+                order.append(module)
+            registered_hook = m.register_forward_pre_hook(forward_pre_hook)
+            hooks.append(registered_hook)
+        model.apply(add_hook)
+        model(*example_input)
+        [hook.remove() for hook in hooks]
+        return order
+    
+    @classmethod
+    def get_submodule_inputs(cls, model: Module, *example_input) -> List[Any]:
+        inputs = []
+        hooks = []
+        def add_hook(m: Module):
+            def forward_pre_hook(module: Module, input):
+                inputs.append(input)
+            registered_hook = m.register_forward_pre_hook(forward_pre_hook)
+            hooks.append(registered_hook)
+        model.apply(add_hook)
+        model(*example_input)
+        [hook.remove() for hook in hooks]
+        return inputs    
+
+    @classmethod
+    def get_name_input_mapping(cls, m: Module, *example_input: Any):
+        names = cls.get_names_order(m, *example_input)
+        inputs = cls.get_submodule_inputs(m, *example_input)
+        return dict(zip(names, inputs))
+
+    @staticmethod
+    def is_leaf_module(m: Module) -> bool:
+        for _ in m.children():
+            return False
+        return True
