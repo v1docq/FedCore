7ca8c5a853a2d2f80a14630cd9963b2dbbae36f7
leostre <strelllleo@mail.ru>
2025-03-21 19:48:14 +0300

    config with validation

---
+++ fedcore/api/api_configs.py
@@ -0,0 +1,120 @@
+from enum import Enum
+from functools import reduce
+from inspect import signature, isclass
+from typing import get_origin, get_args, Literal, Optional, Union
+
+from torch.ao.quantization.utils import _normalize_kwargs
+from torch.nn import Module
+
+from fedcore.repository.constanst_repository import Schedulers, Optimizers, PEFTStrategies
+
+
+__all__ = [
+    'ConfigTemplate',
+    'DeviceConfigTemplate',
+    'EdgeConfigTemplate',
+    'AutoMLConfigTemplate',
+    'NeuralModelConfigTemplate',
+    'LearningConfigTemplate',
+    'APIConfigTemplate',
+]
+
+def get_nested(root: object, k: str):
+    *path, last = k.split('.')
+    return reduce(getattr, path, root), last
+    
+
+class ConfigTemplate:
+    __slots__ = tuple()
+
+    @classmethod
+    def get_default_name(cls):
+        return cls.__name__.split('.')[-1][:-8]
+
+    @classmethod
+    def get_annotation(cls, key):
+        obj = cls if ConfigTemplate in cls.__bases__ else cls.__bases__[0]
+        return signature(obj.__init__).parameters[key].annotation
+        
+    @classmethod
+    def check(cls, key, val):
+        annotation = cls.get_annotation(key)
+        if isclass(annotation):
+            if issubclass(annotation, Enum) and val is not None and not hasattr(annotation, val):
+                raise ValueError(f'`{val}` not supported as {key}. Options: {annotation._member_names_}')
+            elif not isinstance(val, annotation):
+                raise TypeError(f'`Passed type {val.__class__}. Expected: {annotation}')
+        if get_origin(annotation) is Literal and not val in get_args(annotation):
+            raise ValueError(f'Passed value `{val}`. Supported: {get_args(annotation)}')
+
+    def __new__(cls, *args, **kwargs):
+        allowed_parameters = _normalize_kwargs(cls.__init__, kwargs)
+        for k in kwargs:
+            if k not in allowed_parameters:
+                raise KeyError(f'Unknown field `{k}` was passed into {cls.__name__}')
+        sign_args = tuple(signature(cls.__init__).parameters)
+        complemented_args = dict(zip(sign_args[1:],
+            args))
+        allowed_parameters.update(complemented_args)
+        return cls, allowed_parameters
+
+class DeviceConfigTemplate(ConfigTemplate):
+    # """Backend specifications"""
+    def __init__(self,
+        device: Literal['cuda', 'cpu', 'gpu'] = 'cuda',
+        inference: Literal['onnx'] = 'onnx',
+    ): pass
+    
+class EdgeConfigTemplate(ConfigTemplate):
+    # """Backend specifications"""
+    def __init__(self,
+        device: Literal['cuda', 'cpu', 'gpu'] = 'cuda',
+        inference: Literal['onnx'] = 'onnx',
+    ): pass
+
+class AutoMLConfigTemplate(ConfigTemplate):
+    """ """
+    def __init__(self,
+        timeout: int = 10,
+        pop_size: int = 5,  
+        early_stopping_iterations: int = 10,
+        early_stopping_timeout: int = 10,
+        with_tuning: bool = False,
+        n_jobs: int = -1,    
+        initial_assumption: Union[Module, str] = None,
+        optimizer: dict = {'optimisation_strategy':
+                          {'mutation_agent': 'random',
+                           'mutation_strategy': "params_mutation_strategy"},
+                      'optimisation_agent': 'Fedcore'},
+        optimization_agent: Literal['Fedcore'] = 'Fedcore',
+        mutation_strategy: Literal['params_mutation_strategy'] = 'params_mutation_strategy',
+    ): pass
+
+class NeuralModelConfigTemplate(ConfigTemplate):
+    def __init__(self, 
+        epochs: int = 15, 
+        learning_rate: float = 0.0001,
+        optimizer: Optimizers = 'adam',
+        scheduler: Optional[Schedulers] = None,
+    ): pass 
+
+class LearningConfigTemplate(ConfigTemplate):
+    def __init__(self, 
+        learning_strategy: Literal['from_scratch', ] = 'from_scratch',
+        peft_strategy: PEFTStrategies = 'training',
+        peft_strategy_params: str = '',
+        learning_params: NeuralModelConfigTemplate = None,
+    ): pass
+
+class ComputeConfigTemplate(ConfigTemplate):
+    def __init__(self,
+        some_dict: dict = None
+    ): pass
+
+class APIConfigTemplate(ConfigTemplate):
+    def __init__(self, 
+        device_config: DeviceConfigTemplate = None,
+        automl_config: AutoMLConfigTemplate = None,
+        learning_config: LearningConfigTemplate = None,
+        compute_config: ComputeConfigTemplate = None
+    ): pass
---
+++ fedcore/api/config_factory.py
@@ -0,0 +1,111 @@
+from enum import Enum
+from inspect import signature, isclass
+from typing import get_args, get_origin, Literal, Optional, Union
+
+from .api_configs import ConfigTemplate
+
+
+__all__ = [
+    'ConfigFactory',
+]
+
+        
+class ConfigFactory:
+    registered_configs: dict = {}
+
+    def __new__(*ars, **kwargs):
+        raise Exception('ConfigFactory is a static class')
+
+    @classmethod
+    def from_template(cls, template: ConfigTemplate, name:str = None):
+        template_cls, content = template
+        if name is None:
+            name = template_cls.get_default_name()
+        cls.registered_configs[name] = template_cls
+        slots = tuple(content)
+        def __init__(self):
+            for key, value in content.items():
+                value, need_check = ConfigFactory._instantiate_default(name, key, value)
+                if not need_check:
+                    self.__class__.check(key, value)
+                    setattr(self, key, value)
+                else:
+                    object.__setattr__(self, key, value)
+
+        def __getitem__(self, key):
+            val = getattr(self, key)
+            if isinstance(val, Enum):
+                return val.value
+            return val
+
+        def __new__(cls, *args, **kwargs):
+            return object.__new__(cls, *args, **kwargs)
+        
+        def __setattr__(self: ConfigTemplate, key, value):
+            self.check(key, value)
+            ConfigTemplate.__setattr__(self, key, value)
+        
+        def __setitem__(self, key, value):
+            orig_type = type(getattr(self, key))
+            if not isinstance(value, orig_type): #isinstance or is?
+                raise ValueError(f'Passing wrong argument of type {value.__class__.__name__}! Required: {orig_type}')
+            setattr(self, key, value)
+
+        def update(self, d: dict):
+            for k, v in d.items():
+                obj, attr = get_nested(self, k)
+                obj.__setattr__(attr, v)
+
+        def get(self, key, default=None):
+            return getattr(*get_nested(self, key), default)
+        
+        class_dict = {
+            '__slots__': slots,
+            '__init__': __init__,
+            '__new__': __new__,
+            '__getitem__': __getitem__,
+            '__setitem__': __setitem__,
+            '__setattr__': __setattr__,
+            'get': get,
+            'update': update,
+        }
+        return type(name, (cls.registered_configs[name],), class_dict)
+    
+    @classmethod
+    def _get_annotation(cls, config_name, key):
+        template_cls = cls.registered_configs[config_name]
+        return signature(template_cls.__init__).parameters[key].annotation
+    
+    @staticmethod
+    def __is_optional(annotation):
+        origin = get_origin(annotation)
+        return ((origin is Union or origin is Literal) and 
+           type(None) in get_args(annotation))         
+    
+    @classmethod
+    def _instantiate_default(cls, config_name: str, k: str, v):
+        if v is not None:
+            return v, True
+        # check if explicitly optional
+        annotation = cls._get_annotation(config_name, k)
+        if cls.__is_optional(annotation):
+            return None, False
+        # if another config, run recursively
+        if isclass(annotation) and issubclass(annotation, ConfigTemplate):
+            return ConfigFactory.from_template(annotation(), annotation.get_default_name())(), False
+        # try instantiate empty object
+        x = ValueError(f'Misconfiguration! detected class `{annotation}`\
+                        has wrong argument passed and not explicitly optional.')
+        # case union
+        for arg in get_args(annotation):
+            try:
+                return arg(), False
+            except:
+                raise x
+        # case type
+        if isclass(annotation):
+            try:
+                return annotation(), False
+            except:
+                raise x
+        raise x
