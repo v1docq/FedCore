06898bf58c004ca01cf3ecf528845fdf6e568a8d
leostre <strelllleo@mail.ru>
2025-04-14 11:56:33 +0300

    deleted extra init asm, why error in build graph?

--- fedcore/api/main.py
+++ fedcore/api/main.py
@@ -15,10 +15,8 @@ from fedot.core.pipelines.pipeline import Pipeline
 from pymonad.either import Either
 from pymonad.maybe import Maybe
 from torch import Tensor
-from torch.utils.data import DataLoader
 from fedcore.api.utils.checkers_collection import DataCheck
 from fedcore.architecture.abstraction.decorators import DaskServer, exception_handler
-from fedcore.data.data import CompressionInputData
 from fedcore.inference.onnx import ONNXInferenceModel
 from fedcore.neural_compressor.config import Torch2ONNXConfig
 from fedcore.repository.constanst_repository import (
@@ -65,7 +63,6 @@ class FedCore(Fedot):
                 'mutation_strategy': self.manager.automl_config.mutation_strategy,
                 'mutation_agent': self.manager.automl_config.mutation_agent})
             self.manager.automl_config.optimizer = fedcore_opt
-            self.manager.automl_config.fedot_config.optimizer = fedcore_opt
             # self.manager.automl_config.config.update({'optimizer': fedcore_opt})
         return input_data
 
@@ -75,7 +72,8 @@ class FedCore(Fedot):
                                     use_input_preprocessing=False,
                                     use_auto_preprocessing=False)
         initial_assumption = FEDOT_ASSUMPTIONS[self.manager.learning_config.peft_strategy]
-        initial_assumption = initial_assumption(params=self.manager.learning_config.peft_strategy_params.to_dict())
+        initial_assumption = initial_assumption(
+            params=self.manager.learning_config.peft_strategy_params.to_dict())
         initial_pipeline = initial_assumption.build()
         self.manager.solver.params.data.update({'initial_assumption': initial_pipeline})
         return input_data
@@ -95,8 +93,8 @@ class FedCore(Fedot):
         self.manager.solver = Fedot(**self.manager.automl_config.fedot_config,
                                     use_input_preprocessing=False,
                                     use_auto_preprocessing=False)
-        initial_assumption = FEDOT_ASSUMPTIONS[self.manager.learning_config.peft_strategy]()
-        initial_assumption.heads[0].parameters = self.manager.learning_config.peft_strategy_params.to_dict()
+        initial_assumption = FEDOT_ASSUMPTIONS[self.manager.learning_config.peft_strategy](params=self.manager.learning_config.peft_strategy_params.to_dict())
+        # initial_assumption.heads[0].parameters = self.manager.learning_config.peft_strategy_params.to_dict()
         self.manager.solver = initial_assumption.build()
         return input_data
 
@@ -124,7 +122,7 @@ class FedCore(Fedot):
         predict = self.fedcore_model.predict(predict_data, output_mode)
         return predict
 
-    def fit(self, input_data: CompressionInputData, manually_done: bool = False, **kwargs):
+    def fit(self, input_data: tuple, manually_done: bool = False, **kwargs):
         """
         Method for training Industrial model.
 
@@ -135,11 +133,10 @@ class FedCore(Fedot):
         """
 
         def fit_function(train_data):
-            pretrain_before_optimise = self.manager.learning_config.config['learning_strategy'].__contains__(
-                'scratch')
-            model_learning_pipeline = FEDOT_ASSUMPTIONS["training"]()
-            model_learning_pipeline.heads[
-                0].parameters = self.manager.learning_config.learning_strategy_params.to_dict()
+            model_learning_pipeline = FEDOT_ASSUMPTIONS["training"](
+                params=self.manager.learning_config.config.learning_strategy_params.to_dict()
+            )
+            pretrain_before_optimise = self.manager.learning_config.learning_strategy == 'from_scratch'
             model_learning_pipeline = model_learning_pipeline.build()
             pretrain_func = lambda data: self._pretrain_before_optimise(model_learning_pipeline, data) \
                 if pretrain_before_optimise else data
@@ -226,8 +223,8 @@ class FedCore(Fedot):
 
     def evaluate_metric(
             self,
-            prediction: OutputData,
-            target: DataLoader,
+            predicton: Union[Tensor, np.array],
+            target: Union[list, np.array],
             problem: str = "computational",
     ) -> pd.DataFrame:
         """
@@ -247,41 +244,29 @@ class FedCore(Fedot):
             pandas DataFrame with calculated metrics
 
         """
-        is_inference_metric = problem.__contains__("computational")
-        is_fedcore_model = problem.__contains__('fedcore')
-        model_regime = 'model_after' if is_fedcore_model else 'model_before'
 
-        def preproc_predict(prediction):
-            prediction = prediction.predict
-            model_output = prediction.cpu().detach().numpy() if isinstance(prediction, Tensor) else prediction
-            model_output_is_probs = all([len(model_output.shape) > 1, model_output.shape[1] > 1])
-            if model_output_is_probs and not self.manager.automl_config.fedot_config.problem.__contains__(
-                    'forecasting'):
-                labels = np.argmax(model_output, axis=1)
-                predicted_probs = model_output
+        model_output = predicton.cpu().detach().numpy() if isinstance(predicton, Tensor) else predicton
+        model_output_is_probs = all([len(model_output.shape) > 1, model_output.shape[1] > 1])
+        if model_output_is_probs and not self.manager.automl_config.fedot_config.problem.__contains__('forecasting'):
+            labels = np.argmax(model_output, axis=1)
+            predicted_probs = model_output
+        else:
+            labels = model_output
+            predicted_probs = model_output
+
+        inference_metric = problem.__contains__("computational")
+        if inference_metric:
+            if problem.__contains__('fedcore'):
+                model_regime = 'model_after'
             else:
-                labels = model_output
-                predicted_probs = model_output
-            return labels, predicted_probs
-
-        def preproc_target(target):
-            if hasattr(target, 'targets'):
-                target = target.dataset.targets
-            else:
-                iter_object = iter(target.dataset)
-                target = np.array([batch[1] for batch in iter_object])
-            return target
-
-        preproc_labels, preproc_probs = preproc_predict(prediction)
-        preproc_target = preproc_target(target)
-        if is_inference_metric:
+                model_regime = 'model_before'
             prediction_dict = dict(model=self.fedcore_model, dataset=target, model_regime=model_regime)
         else:
-            prediction_dict = dict(target=preproc_target, labels=preproc_labels, probs=preproc_probs)
+            prediction_dict = dict(target=target, labels=labels, probs=predicted_probs)
         prediction_dataframe = FEDOT_GET_METRICS[problem](**prediction_dict)
         return prediction_dataframe
 
-    def get_report(self, test_data: CompressionInputData):
+    def get_report(self, test_data: InputData):
         def create_df(iterator):
             df_list = []
             for metric_dict, col in iterator:
@@ -290,36 +275,19 @@ class FedCore(Fedot):
                 df_list.append(df)
             return pd.concat(df_list, axis=1)
 
-        def calculate_metric_changes(metric_df: pd.DataFrame, metric: list = None):
-            for metric_name in metric:
-                change_val = (metric_df[f'original_{metric_name}'] - metric_df[f'fedcore_{metric_name}']) / \
-                             metric_df[f'original_{metric_name}'] * 100
-                change_val = round(change_val, 1)
-                if metric_name == 'throughput':
-                    change_val = abs(change_val)
-                metric_df[f'{metric_name}_change'] = change_val
-            return metric_df
-
         eval_regime = ['original', 'fedcore']
         prediction_list = [self.predict(test_data, output_mode=mode) for mode in eval_regime]
-        quality_metrics_list = [self.evaluate_metric(prediction=prediction.predict,
-                                                     target=test_data.val_dataloader,
+        quality_metrics_list = [self.evaluate_metric(predicton=prediction.predict,
+                                                     target=test_data.target,
                                                      problem=self.manager.automl_config.fedot_config.problem)
                                 for prediction in prediction_list]
-        computational_metrics_list = [self.evaluate_metric(prediction=prediction.predict,
+        computational_metrics_list = [self.evaluate_metric(predicton=prediction,
                                                            target=test_data.val_dataloader,
                                                            problem=f'computational_{regime}')
                                       for prediction, regime in zip(prediction_list, eval_regime)]
 
         quality_df = create_df(zip(quality_metrics_list, eval_regime))
         compute_df = create_df(zip(computational_metrics_list, eval_regime))
-        problem = self.manager.automl_config.fedot_config.problem
-        if any([problem == 'ts_forecasting', problem == 'regression']):
-            quality_metric = 'rmse'
-        else:
-            quality_metric = 'accuracy'
-        quality_df = calculate_metric_changes(quality_df, metric=[quality_metric])
-        compute_df = calculate_metric_changes(compute_df, metric=['latency', 'throughput'])
         return dict(quality_comparasion=quality_df, computational_comparasion=compute_df)
 
     def load(self, path):
@@ -401,11 +369,9 @@ class FedCore(Fedot):
 
     def shutdown(self):
         """Shutdown Dask client"""
-        # if self.manager.dask_client is not None:
-        if hasattr(self.manager, 'dask_client'):
+        if self.manager.dask_client is not None:
             self.manager.dask_client.close()
             del self.manager.dask_client
-        if hasattr(self.manager, 'dask_cluster'):
-        # if self.manager.dask_cluster is not None:
+        if self.manager.dask_cluster is not None:
             self.manager.dask_cluster.close()
             del self.manager.dask_cluster
