7298dd5d9734a77c82d1a0fb00c7db438f7cf959
Irina Usoltseva <irinaslcv@gmail.com>
2025-07-16 15:30:49 +0300

    Updated DynamicRankPruner

--- fedcore/algorithm/low_rank/hooks.py
+++ fedcore/algorithm/low_rank/hooks.py
@@ -5,7 +5,7 @@ import torch
 from numpy import diff, abs as npabs
 
 
-from fedcore.algorithm.low_rank.rank_pruning import rank_threshold_pruning
+from fedcore.algorithm.low_rank.rank_pruning import rank_threshold_pruning, rank_attr_pruning
 from fedcore.architecture.abstraction.accessor import Accessor
 from fedcore.models.network_impl.hooks import BaseHook
 from fedcore.models.network_impl.decomposed_layers import IDecomposed
@@ -72,7 +72,7 @@ class DynamicRankPruner(BaseHook):
 
     def _prepare_mapping(self):
         """Returns initial rank of weight matrices estimated via SVD"""
-        return {name: [torch.sum((S > self.sv_thr))] for name, S in self._S_gen(self.model)}
+        return {name: [torch.sum((S > self.sv_thr))] + [0] * (self.n_plateau - 1) for name, S in self._S_gen(self.model)}
     
     def _get_ksis(self):
         return {name: self.traced_layers[name][0] / self._estimate_stable_rank(S) for name, S in self._S_gen(self.model)}
@@ -80,19 +80,16 @@ class DynamicRankPruner(BaseHook):
     def _estimate_stable_rank(self, S: torch.Tensor):
         return (S ** 2).sum() / S.max() ** 2
     
-    def _update_stable_ranks(self):
+    def _update_stable_ranks(self, epoch):
         for name, history in self.traced_layers.items():
-            history.append(
-                self.traced_layers_ksis[name] * self._estimate_stable_rank(
-                    Accessor.get_module(self.model, name)
-                )
-            )
+            history[epoch % self.n_plateau] = self.traced_layers_ksis[name] * self._estimate_stable_rank(Accessor.get_module(self.model, name))
+
 
     def trigger(self, epoch, kws) -> dict:
-        self._estimate_stable_rank()
+        self._update_stable_ranks()
         to_prune = {}
         for n, history in self.traced_layers:
-            if (npabs(diff(history[-max(len(history), self.n_plateau + 1):])) < self.pl_thr).all():
+            if (npabs(diff(history)) < self.pl_thr).all():
                 to_prune[n] = ceil(history[-1])
         self.trigger_result = to_prune
         return to_prune
@@ -102,7 +99,14 @@ class DynamicRankPruner(BaseHook):
             layer_name = '.'.join(name.split('.')[:-1])
             layer = Accessor.get_module(self.model, layer_name)
             setattr(layer, self.rank_attr, rank)
-            self.traced_layers.pop(name, None)  
+            strategy = self.params.get('strategy', 'explained_variance')
+            if isinstance(layer, IDecomposed): 
+                rank_attr_pruning(decomposed_module=layer,
+                                    strategy=strategy,
+                                    module_name=layer_name,
+                                    rank_attr=rank)
+            layer.compose_weight_for_inference() 
+            
 
 class LRHooks(Enum):
     onetime = OnetimeRankPruner
