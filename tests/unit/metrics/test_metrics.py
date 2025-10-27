af345f59b5ec9c668c8eb33b3098757496f86f27
leostre <strelllleo@mail.ru>
2025-10-27 18:29:09 +0300

    test: MetricFactory

--- tests/unit/metrics/test_metrics.py
+++ tests/unit/metrics/test_metrics.py
@@ -26,16 +26,10 @@ def test_probs():
     assert mtr.metric(target, preds) is not None, 'wrong `.metric` logic. the type-based decorator didn\'t cope with the input'
 
 
-# def test_metrics_factory_():
-#     target = torch.Tensor([1, 1,])
-#     preds = torch.Tensor([[0, 1], [0.2, 0]])
-#     name_suffix = 'MulticlassPrecision__2'
-#     mtr = MetricFactory.get_metric(name_suffix)
-#     assert name_suffix in LOADED_METRICS, "For each combination metric - type the corresponding class should be created"
-#     assert MetricFactory.get_metric(name_suffix) is mtr, "In case the previously called metric is used, the existing class should be summoned"
-#     assert mtr.metric(target, preds) is not None, 'wrong `.metric` logic'
-#     assert mtr.metric(target, torch.argmax(preds, -1)) is not None, 'wrong `.metric` logic'
+prob_name_suffix = 'MeanSquaredError'
+def test_probs():
+    target = torch.Tensor([1, 1.])
+    preds = torch.Tensor([[0, 1], [0.2, 0]])
+    mtr = MetricFactory.get_metric(prob_name_suffix)
+    assert mtr.metric(target, preds) is not None, 'wrong `.metric` logic. the type-based decorator didn\'t cope with the input'
 
-#     name_suffix = 'MulticlassAUROC__2'
-#     mtr = MetricFactory.get_metric(name_suffix)
-#     assert mtr.metric(target, preds) is not None, 'wrong `.metric` logic. the type-based decorator didn\'t cope with the input'
