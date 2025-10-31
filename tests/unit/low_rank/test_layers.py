059b0413dde7085d8012a2e1f05f56406609d907
Андрей Глазков <glazkoff.andrej2014@yandex.ru>
2025-10-31 23:42:01 +0300

    Minor test refactor and finetune_config depricating in tests

--- tests/unit/low_rank/test_layers.py
+++ tests/unit/low_rank/test_layers.py
@@ -3,7 +3,7 @@ import pytest
 import torch
 import torch.nn as nn 
 
-from fedcore.algorithm.low_rank.svd_tools import decompose_module
+from fedcore.algorithm.low_rank.svd_tools import decompose_module_in_place
 from fedcore.algorithm.quantization.utils import (
     _recreate_decomposed_conv1d, 
     _recreate_decomposed_embedding,
@@ -18,7 +18,7 @@ def test_conv1d():
     in_, out_, k = 18, 29, 3
     C1 = nn.Conv1d(in_, out_, k)
     DC1 = nn.Sequential(deepcopy(C1))
-    decompose_module(DC1, compose_mode='two_layers')
+    decompose_module_in_place(DC1, compose_mode='two_layers')
     DC1[0].compose_weight_for_inference()
     RDC1 = _recreate_decomposed_conv1d(DC1[0])
     X = torch.randn(1, in_, 180)
@@ -29,7 +29,7 @@ def test_conv2d():
     in_, out_, k1, k2 = 18, 29, 3, 5
     C1 = nn.Conv2d(in_, out_, k1, k2)
     DC1 = nn.Sequential(deepcopy(C1))
-    decompose_module(DC1, compose_mode='two_layers', decomposing_mode='channel')
+    decompose_module_in_place(DC1, compose_mode='two_layers', decomposing_mode='channel')
     DC1[0].compose_weight_for_inference()
     RDC1 = _recreate_decomposed_conv2d(DC1[0])
     X = torch.randn(1, in_, 100, 100)
@@ -40,7 +40,7 @@ def test_embedding():
     in_, out_, k = 18, 29, 3
     C1 = nn.Embedding(in_, out_,)
     DC1 = nn.Sequential(deepcopy(C1))
-    decompose_module(DC1, compose_mode='two_layers')
+    decompose_module_in_place(DC1, compose_mode='two_layers')
     DC1[0].compose_weight_for_inference()
     RDC1 = _recreate_decomposed_embedding(DC1[0])
     X = torch.randint(0, in_ - 1, (1, k))
@@ -51,7 +51,7 @@ def test_linear():
     in_, out_, k = 18, 29, 3
     C1 = nn.Linear(in_, out_,)
     DC1 = nn.Sequential(deepcopy(C1))
-    decompose_module(DC1, compose_mode='two_layers')
+    decompose_module_in_place(DC1, compose_mode='two_layers')
     DC1[0].compose_weight_for_inference()
     RDC1 = _recreate_decomposed_linear(DC1[0])
     X = torch.randn(k, in_)
